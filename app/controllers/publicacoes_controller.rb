2d4d84989b9a3e59f2c28710fe76f699207d7a2c
Mr Jones <jones@rogalabs.com>
2019-10-18 14:59:59 -0300

    added more logics
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/controllers/publicacoes_controller.rb
+++ app/controllers/publicacoes_controller.rb
@@ -5,6 +5,11 @@ class PublicacoesController < ApplicationController
     render json: ::Publicacoes::GraphicResultsService.build(@publicacoes, filter_params)
   end
 
+  def general_graphic
+    @publicacoes = Publicacao.all
+    render json: ::Publicacoes::GraphicResultForAllService.build(@publicacoes)
+  end
+
   private
 
   def filter_params
--- app/services/publicacoes/graphic_result_for_all_service.rb
+++ app/services/publicacoes/graphic_result_for_all_service.rb
@@ -1,11 +1,23 @@
 module Publicacoes
-  module GraphicResultsForAllService
+  module GraphicResultForAllService
     class << self
 
       def build(publicacoes)
+        @result = []
+        years = publicacoes.map {|pblc| pblc["ano"]}.uniq
+        areas = Area.all
 
-        
-
+        Instituicao.all.each do |inst|
+          @result.push({ "instituicao": "#{inst.sigla}"})
+          years.each do |year|
+            @result.last.merge!("data": [{"ano": year}])
+            areas.each do |area|
+              binding.pry
+              @result.last[:data].last.merge!("#{area.nome}": Publicacao.where(area_id: area.id, instituicao_id: inst.id).count)
+            end
+          end
+        end
+        @result
       end
 
     end
