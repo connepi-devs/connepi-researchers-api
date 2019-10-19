44a7d0eeaf62e95b2521e898e1440532f8c01a68
Siderlan Santos <siderlan.santos@gmail.com>
2019-10-19 16:12:15 -0300

    Setup endpoint to retrieve chart data

--- app/controllers/publicacoes_controller.rb
+++ app/controllers/publicacoes_controller.rb
@@ -5,6 +5,12 @@ class PublicacoesController < ApplicationController
     render json: ::Publicacoes::GraphicResultsService.build(@publicacoes, filter_params)
   end
 
+  # Retrieve the count of articles of the
+  # Federal Institutes from North and NorthEast
+  def federal_institutes_chart_data
+    render json: ::Publicacoes::Charts::FederalInstitutes.chart_data
+  end
+
   private
 
   def filter_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope :api do
     get 'publicacoes/build_graphic', to: 'publicacoes#build_graphic'
+    get 'publicacoes/institutos_federais_do_norte_e_nordeste', to: 'publicacoes#federal_institutes_chart_data'
 
     resources :instituicoes, only: [:index]
   end
