0867952069f1ca5bf87d33e70fe41780e9a85b13
Junior Duda <duda.ifal@gmail.com>
2019-11-15 14:00:31 -0300

    feature/users-crud fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,11 @@ Rails.application.routes.draw do
         registrations: 'api/v1/auth/registrations',
         sessions: 'api/v1/auth/sessions'
       }
+
+      get 'publications/institutos_federais_do_norte_e_nordeste', to: 'publications#federal_institutes_chart_data'
+      get 'publications/general_graphic', to: 'publications#general_graphic'
+      get 'publications/build_graphic', to: 'publications#build_graphic'
+      resources :institutions, only: [:index]
     end
   end
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # scope :api do
-  #   get 'publicacoes/build_graphic', to: 'publicacoes#build_graphic'
-  #   get 'publicacoes/institutos_federais_do_norte_e_nordeste', to: 'publicacoes#federal_institutes_chart_data'
-  #   get 'publicacoes/general_graphic', to: 'publicacoes#general_graphic'
-  #
-  #   resources :instituicoes, only: [:index]
-  # end
-
 end
