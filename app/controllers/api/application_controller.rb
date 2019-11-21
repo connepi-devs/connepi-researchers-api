7c230cacf69b2a71b4ffca6ece4cbf6349588696
Junior Duda <duda.ifal@gmail.com>
2019-11-20 21:01:54 -0300

    feature/search-article update files

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,5 +1,4 @@
 class Api::ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
-  before_action :authenticate_api_v1_user!
 end
