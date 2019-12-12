62bfdebb816b14bb914da9c85ac271a4ab0741bf
Arthur Novaes <arthurnovaes9@gmail.com>
2019-12-11 22:47:36 -0300

    master Add storage files
    -
    
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_005438) do
+ActiveRecord::Schema.define(version: 2019_12_06_024839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_005438) do
     t.datetime "updated_at", null: false
     t.bigint "area_id"
     t.bigint "instituicao_id"
+    t.string "file_url"
     t.index ["area_id"], name: "index_publicacoes_on_area_id"
     t.index ["instituicao_id"], name: "index_publicacoes_on_instituicao_id"
   end
@@ -113,6 +114,8 @@ ActiveRecord::Schema.define(version: 2019_11_25_005438) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "publicacoes", "areas"
   add_foreign_key "publicacoes", "areas", name: "area_id"
+  add_foreign_key "publicacoes", "instituicoes", column: "instituicao_id"
   add_foreign_key "publicacoes", "instituicoes", column: "instituicao_id", name: "instituicao_id"
 end
